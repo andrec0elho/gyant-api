58346d7015c78ae97f831c45adbcf1039f0f8d97
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-20 17:11:47 +0000

    chore: add dependecies on condition module

--- src/modules/condition/condition.module.ts
+++ src/modules/condition/condition.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConditionController } from './condition.controller';
 import { ConditionService } from './condition.service';
+import { ConditionRepository } from './condition.repository';
+import { ConditionSchema } from './schemas/condition.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Condition', schema: ConditionSchema }])],
   controllers: [ConditionController],
-  providers: [ConditionService]
+  providers: [ConditionService, ConditionRepository],
 })
 export class ConditionModule {}
