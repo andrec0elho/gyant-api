067663f9d84bee390a8b493bce957cf065ecf35a
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-21 17:43:18 +0000

    chore: remove database volume, add read me file with project guide

--- mongodb/dataSetup.js
+++ mongodb/dataSetup.js
@@ -16,7 +16,7 @@ const conditions = data.map(elem => {
 });
 
 // Connection URL
-const url = 'mongodb://root:gyantadmin@localhost:27017/gyant_test?authSource=admin';
+const url = 'mongodb://root:gyantadmin@localhost:27017/gyant?authSource=admin';
 
 const case1Description = `Patient presents with Flank Pain. The patient is a 51-year-old female, no significant past medical history, presents to the emergency department with left-sided flank pain ongoing ×1 month now with abdominal pain. The pain is intermittent, but has been worsening. She reports new onset nausea, vomiting, diarrhea for the last 2 days. She reports multiple episodes of nonbloody emesis starting yesterday. She has also had multiple episodes of nonbloody diarrhea. She has gone to see her primary care doctor twice since symptoms began. She was found to have mildly elevated creatinine and was referred to a nephrologist. However, the nephrologist is not willing to see her until . The patient feels she cannot wait that long especially in light of these new symptoms. She then followed up with her primary care doctor again and he prescribed Zofran and loperamide but offered her no other solutions. The pain has since increased as well. She denies any fevers, chills. She denies urinary symptoms including burning with urination, frequency, hematuria.`;
 const case2Description = `Patient  is an 42 year old  male.    Chief Complaint: Establish Care and Physical    HPI      Hemorrhoids  Bothersome  Comes and goes  Especially with sedentary life style  Recently worse  Couple nights where almost wakes patient up  Gets intermittently constipated  High fiber diet    Patient Active Problem    Diagnoses Code
